a47651f26355a745ce2cdd4bae4964194d462439
alvin-2002 <alvinmaung@gmail.com>
2023-07-22 15:21:35 +1000

    Name Start and End coordinates of chessboard

--- src/client-app/src/models/chess.ts
+++ src/client-app/src/models/chess.ts
@@ -1,5 +1,16 @@
 import { Piece } from "./Pieces/piece";
 
+// Rank (row) '1' 
+export const StartRank: number = 7;
+// File (col) 'a'
+export const StartFile: number = 0;
+
+// Rank (row) '8'
+export const EndRank: number = 0;
+// File (col) 'h'
+export const EndFile: number = 7;
+
+
 export type Color = 'white' | 'black';
 
 export type PieceType = 'rook' | 'knight' | 'bishop' | 'king' | 'queen' | 'pawn';
